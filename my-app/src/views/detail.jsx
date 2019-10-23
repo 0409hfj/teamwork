b0b57afe7de07633e7d8f5030fd1d30c77c948a1
3268769689 <55084517+3268769689@users.noreply.github.com>
2019-10-23 11:37:26 +0800

    发布

--- my-app/src/views/detail.jsx
+++ my-app/src/views/detail.jsx
@@ -8,7 +8,7 @@ export default class Detail extends Component {
                     <span>&lt;</span>
                     <span>投票详情</span> 
                     <span>
-                        <NavLink to="#">统计</NavLink>   
+                        <NavLink to="/wode">统计</NavLink>   
                     </span>
                 </header>
                 <section>
